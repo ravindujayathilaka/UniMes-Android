18fb9b23f09fb0ff72eeb3fe45eb0080b545a84a
rAVINDUpRABASHWARA <00prabashwara123@gmail.com>
2022-04-28 11:56:17 +0530

    Create the first explicit intent in the project between SignInActivity and SignUpActivity

--- app/src/main/java/com/example/unimes/activities/SignUpActivity.java
+++ app/src/main/java/com/example/unimes/activities/SignUpActivity.java
@@ -2,15 +2,26 @@ package com.example.unimes.activities;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
 
 import com.example.unimes.R;
+import com.example.unimes.databinding.ActivitySignUpBinding;
 
 public class SignUpActivity extends AppCompatActivity {
+
+    private ActivitySignUpBinding binding;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_sign_up);
+        binding = ActivitySignUpBinding.inflate(getLayoutInflater());
+        setContentView(binding.getRoot());
+        setListners();
+    }
+
+    private void setListners() {
+        binding.textSignIn.setOnClickListener( v ->
+                startActivity(new Intent(getApplicationContext(), SignInActivity.class)));
     }
 }
