3ac95d79c07530893096b9ccb3afcc6c9c7db427
rAVINDUpRABASHWARA <00prabashwara123@gmail.com>
2022-04-28 10:05:30 +0530

    Create Sign in and Sign out Activities for the UniMes

---
+++ app/src/main/java/com/example/unimes/activities/SignInActivity.java
@@ -0,0 +1,16 @@
+package com.example.unimes.activities;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+import com.example.unimes.R;
+
+public class SignInActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_sign_in);
+    }
+}
