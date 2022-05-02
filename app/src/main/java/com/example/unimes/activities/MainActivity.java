ce99bcc9eed4ca62d60656a753f6194935eaf52c
rAVINDUpRABASHWARA <00prabashwara123@gmail.com>
2022-05-02 11:18:12 +0530

    Showing the Profile image and User name and Logout btn

--- app/src/main/java/com/example/unimes/activities/MainActivity.java
+++ app/src/main/java/com/example/unimes/activities/MainActivity.java
@@ -2,16 +2,35 @@ package com.example.unimes.activities;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
 import android.os.Bundle;
+import android.util.Base64;
 
 import com.example.unimes.R;
+import com.example.unimes.databinding.ActivityMainBinding;
+import com.example.unimes.utilities.Constants;
+import com.example.unimes.utilities.PreferenceManager;
 
 public class MainActivity extends AppCompatActivity {
+
+    private ActivityMainBinding binding;
+    private PreferenceManager preferenceManager;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+        binding = ActivityMainBinding.inflate(getLayoutInflater());
+        setContentView(binding.getRoot());
+        preferenceManager = new PreferenceManager(getApplicationContext());
+        loadingDetails();
+    }
+
+    private void loadingDetails() {
+        binding.textName.setText(preferenceManager.getString(Constants.KEY_NAME));
+        byte[] bytes = Base64.decode(preferenceManager.getString(Constants.KEY_IMAGE), Base64.DEFAULT);
+        Bitmap bitmap = BitmapFactory.decodeByteArray(bytes, 0, bytes.length);
+        binding.imageProfile.setImageBitmap(bitmap);
     }
 }
 //Test
