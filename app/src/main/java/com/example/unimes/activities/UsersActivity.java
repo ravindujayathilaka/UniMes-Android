15930f81aee75c1540eed7ade954358617f521d7
rAVINDUpRABASHWARA <00prabashwara123@gmail.com>
2022-05-02 19:33:58 +0530

    Create select user page

---
+++ app/src/main/java/com/example/unimes/activities/UsersActivity.java
@@ -0,0 +1,86 @@
+package com.example.unimes.activities;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.graphics.drawable.Drawable;
+import android.os.Bundle;
+import android.service.controls.actions.BooleanAction;
+import android.view.View;
+
+import com.example.unimes.adapters.UsersAdapter;
+import com.example.unimes.databinding.ActivityMainBinding;
+import com.example.unimes.databinding.ActivityUsersBinding;
+import com.example.unimes.models.User;
+import com.example.unimes.utilities.Constants;
+import com.example.unimes.utilities.PreferenceManager;
+import com.google.firebase.firestore.FirebaseFirestore;
+import com.google.firebase.firestore.QueryDocumentSnapshot;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class UsersActivity extends AppCompatActivity {
+
+    private ActivityUsersBinding binding;
+    private PreferenceManager preferenceManager;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        binding = ActivityUsersBinding.inflate(getLayoutInflater());
+        setContentView(binding.getRoot());
+        preferenceManager = new PreferenceManager(getApplicationContext());
+        setlisteners();
+        getUsers();
+    }
+
+    private void setlisteners() {
+        binding.imageBack.setOnClickListener( v -> onBackPressed());
+    }
+
+    private void getUsers() {
+        loading(true);
+        FirebaseFirestore database = FirebaseFirestore.getInstance();
+        database.collection(Constants.KEY_COLLECTION_USERS)
+                .get()
+                .addOnCompleteListener(task -> {
+                    loading(false);
+                    String currentUserId = preferenceManager.getString(Constants.KEY_USER_ID);
+                    if(task.isSuccessful() && task.getResult() != null ) {
+                        List<User> users = new ArrayList<>();
+                        for (QueryDocumentSnapshot queryDocumentSnapshot : task.getResult()) {
+                            if(currentUserId.equals(queryDocumentSnapshot.getId())) {
+                                continue;
+                            }
+                            User user = new User();
+                            user.name = queryDocumentSnapshot.getString(Constants.KEY_NAME);
+                            user.email = queryDocumentSnapshot.getString(Constants.KEY_EMAIL);
+                            user.image = queryDocumentSnapshot.getString(Constants.KEY_IMAGE);
+                            user.token = queryDocumentSnapshot.getString(Constants.KEY_FCM_TOKEN);
+                            users.add(user);
+                        }
+                        if (users.size() > 0){
+                            UsersAdapter usersAdapter = new UsersAdapter(users);
+                            binding.usersRecyclerView.setAdapter(usersAdapter);
+                            binding.usersRecyclerView.setVisibility(View.VISIBLE);
+                        } else {
+                            showErrorMessage();
+                        }
+                    } else {
+                        showErrorMessage();
+                    }
+                });
+    }
+
+    private void showErrorMessage() {
+        binding.textErrorMessage.setText(String.format("%s", "No user available"));
+        binding.textErrorMessage.setVisibility(View.VISIBLE);
+    }
+    private void loading(Boolean isloading) {
+        if(isloading) {
+            binding.prograssBar.setVisibility(View.VISIBLE);
+        } else {
+            binding.prograssBar.setVisibility(View.INVISIBLE);
+        }
+    }
+}
---
+++ app/src/main/java/com/example/unimes/models/User.java
@@ -0,0 +1,8 @@
+package com.example.unimes.models;
+
+import java.io.Serializable;
+
+public class User implements Serializable {
+
+    public String name, email, image, token;
+}
