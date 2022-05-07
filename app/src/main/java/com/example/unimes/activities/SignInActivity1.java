a1b76032136ef8d7a8e9f14df4c98b2679fa700b
janangar <91012464+janangar@users.noreply.github.com>
2022-05-07 18:41:14 +0530

    Finished sign in page function

--- app/src/main/java/com/example/unimes/activities/SignInActivity1.java
+++ app/src/main/java/com/example/unimes/activities/SignInActivity1.java
@@ -54,20 +54,73 @@ public class SignInActivity1 extends AppCompatActivity {
         });
     }
 
-
-    //Testing the data sending
-    private void addDataToFirebase() {
+    private void signIn() {
+        loading(true);
         FirebaseFirestore database = FirebaseFirestore.getInstance();
-        HashMap<String, Object> data = new HashMap<>();
-        data.put("First_Name", "Ravindu");
-        data.put("Last_Name", "Jayathilaka");
-        database.collection("users")
-                .add(data)
-                .addOnSuccessListener(documentReference -> {
-                    Toast.makeText(getApplicationContext(), "Data Inserted", Toast.LENGTH_SHORT).show();
-                })
-                .addOnFailureListener(exception ->{
-                    Toast.makeText(getApplicationContext(), exception.getMessage(), Toast.LENGTH_SHORT).show();
+        database.collection(Constants.KEY_COLLECTION_USERS)
+                .whereEqualTo(Constants.KEY_EMAIL, binding.inputEmail.getText().toString())
+                .whereEqualTo(Constants.KEY_PASSWORD, binding.inputPassword.getText().toString())
+                .get()
+                .addOnCompleteListener(task -> {
+                    if (task.isSuccessful() && task.getResult() != null
+                            && task.getResult().getDocuments().size() > 0) {
+                        DocumentSnapshot documentSnapshot = task.getResult().getDocuments().get(0);
+                        preferenceManager.putBoolean(Constants.KEY_IS_SIGNED_IN, true);
+                        preferenceManager.putString(Constants.KEY_USER_ID, documentSnapshot.getId());
+                        preferenceManager.putString(Constants.KEY_NAME, documentSnapshot.getString(Constants.KEY_NAME));
+                        preferenceManager.putString(Constants.KEY_IMAGE, documentSnapshot.getString(Constants.KEY_IMAGE));
+                        Intent intent = new Intent(getApplicationContext(), MainActivity.class);
+                        intent.addFlags(Intent.FLAG_ACTIVITY_NEW_TASK | Intent.FLAG_ACTIVITY_CLEAR_TASK);
+                        startActivity(intent);
+                    }else {
+                        loading(true);
+                        showToast("Unable to sign in");
+                    }
                 });
     }
 
+
+    private void loading(Boolean isloading) {
+        if(isloading) {
+            binding.buttonsignin.setVisibility(View.INVISIBLE);
+            binding.prograssBar.setVisibility(View.VISIBLE);
+        }else {
+            binding.buttonsignin.setVisibility(View.VISIBLE);
+            binding.prograssBar.setVisibility(View.INVISIBLE);
+        }
+    }
+    //Testing the data sending
+//    private void addDataToFirebase() {
+//        FirebaseFirestore database = FirebaseFirestore.getInstance();
+//        HashMap<String, Object> data = new HashMap<>();
+//        data.put("First_Name", "Ravindu");
+//        data.put("Last_Name", "Jayathilaka");
+//        database.collection("users")
+//                .add(data)
+//                .addOnSuccessListener(documentReference -> {
+//                    Toast.makeText(getApplicationContext(), "Data Inserted", Toast.LENGTH_SHORT).show();
+//                })
+//                .addOnFailureListener(exception ->{
+//                    Toast.makeText(getApplicationContext(), exception.getMessage(), Toast.LENGTH_SHORT).show();
+//                });
+//    }
+
+    private void showToast(String message) {
+        Toast.makeText(getApplicationContext(), message, Toast.LENGTH_SHORT).show();
+    }
+
+    private Boolean isValidSignDetails() {
+        if(binding.inputEmail.getText().toString().trim().isEmpty()) {
+            showToast("Enter Email");
+            return false;
+        } else if (!Patterns.EMAIL_ADDRESS.matcher(binding.inputEmail.getText().toString()).matches()) {
+            showToast("Enter Valid Email");
+            return false;
+        } else if (binding.inputPassword.getText().toString().isEmpty()) {
+            showToast("Enter Password");
+            return false;
+        } else {
+            return true;
+        }
+    }
+}
